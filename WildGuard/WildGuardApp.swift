01ebdccf5666aa8b79a203113013b46ecc182e59
Steve Hong <stevehonggg@gmail.com>
2024-02-13 16:46:58 +0900

    Initial Commit

---
+++ WildGuard/WildGuardApp.swift
@@ -0,0 +1,27 @@
+//
+//  WildGuardApp.swift
+//  WildGuard
+//
+//  Created by Paul Hudson on 27/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct WildGuardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        
+        WindowGroup(for: String.self) { $string in
+            if let string {
+                Image(string)
+                    .resizable()
+                    .scaledToFill()
+            }
+        }
+        .defaultSize(width: 1000, height: 1000)
+    }
+}
+
